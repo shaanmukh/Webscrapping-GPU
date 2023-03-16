17708cb5db1a9d683d7ad5e8c871f77320d28c0f
shaanmukh <99368858+shaanmukh@users.noreply.github.com>
2023-03-16 11:13:01 -0500

    added number of pages

--- webscrapping.py
+++ webscrapping.py
@@ -13,5 +13,11 @@ doc = BeautifulSoup(page, "html.parser")
 page_text = doc.find(class_="list-tool-pagination-text").strong
 
 
-print(page_text)
+pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
+
+for page in range(pages):
+    url = f"https://www.newegg.com/p/pl?d={gpu}&N=4131&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+
 
